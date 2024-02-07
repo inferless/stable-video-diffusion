5b7cec15d94679650f43471531c980b55ea0c109
rbgo404 <150957746+rbgo404@users.noreply.github.com>
2024-02-07 10:38:00 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "image_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://images.cnbctv18.com/wp-content/uploads/2022/08/ashneer-grover-3-Meme-1-1019x573.jpg"]
+    }
+}
